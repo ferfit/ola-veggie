aaa465aa19a2ebb54d9f57d76e1b9165fc930653
Fernando Pereyra <fpereyra@inxap.com>
2022-04-17 12:14:08 -0300

    agrega seccion beneficio

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@
         console.log($videoSrc);
 
         $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+            $("#video").attr('src', $videoSrc + "?modestbranding=1&amp;showinfo=0");
         })
 
         $('#videoModal').on('hide.bs.modal', function (e) {
