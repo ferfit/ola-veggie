af4907a62c8d554d85636e581ddfeda6b364b136
Fernando Pereyra <fpereyra@inxap.com>
2022-04-28 17:19:12 -0300

    cambia imagenes

--- mail/contact.js
+++ mail/contact.js
@@ -8,19 +8,19 @@ $(function () {
             event.preventDefault();
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var subject = $("input#subject").val();
+            var telefono = $("input#telefono").val();
             var message = $("textarea#message").val();
 
             $this = $("#sendMessageButton");
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "mail/contact.php",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
-                    subject: subject,
+                    telefono: telefono,
                     message: message
                 },
                 cache: false,
@@ -29,7 +29,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Tu mensaje se ha enviado con exito. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
@@ -38,7 +38,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Lo sentimos " + name+ ", parece que nuestro servidor de correo no responde. ¡Por favor, inténtelo de nuevo más tarde!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
--- mail/contact.php
+++ mail/contact.php
@@ -1,19 +1,24 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['telefono']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$telefono = strip_tags(htmlspecialchars($_POST['telefono']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "ferfit16@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$subject = "Nuevo mensaje - Formulario landingpage";
+$body = "Tienes un nuevo mensaje desde el formulario de contacto de tu landingpage. Estos son los datos:\n\n"."
+Nombre: $name\n\n\n
+Email: $email\n\n
+Teléfono: $telefono\n\n
+Mensaje: $message";
+
 $header = "From: $email";
-$header .= "Reply-To: $email";	
+//$header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
